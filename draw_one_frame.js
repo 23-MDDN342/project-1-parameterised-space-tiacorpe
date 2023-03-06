a596f174b87fb95a85ab097521dd31779d89358e
Tia <leticia.corpe@gmail.com>
2023-03-07 08:42:21 +1300

    Early Stages

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -2,6 +2,13 @@
 function draw_one_frame(cur_frac) {
   //background(255);
 
+  // outer circle
+  ellipse(width/2, height/2, height, height);
 
+  // inner circle
+  ellipse(width/2, height/2, height/1.2, height/1.2);
+
+
+  ellipse(width/1, height/4, height/2, height/2);
 
 }
