0457416a0677c78d51f75cb39f8654c40752db87
Tia <leticia.corpe@gmail.com>
2023-03-09 09:45:15 +1300

    Cogs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300842&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
 
-5/3/2023
-Still in the very early stages and just getting used to working in regards to height and width. Going with a geometric design with clocks but just trying to get shapes on the page at this stage. Also having trouble as VS code isn't auto-completing the p5.js code, so will most likely fix that before moving any further.
+9/3/2023
+Still in the very early stages and largely just trying to focus on the movements of certain elements. I got the cog shape with the help of ChatGPT. For the rotation, I'm still experimenting with the rotation rate to make things loop nicely! Also trying to figure out the best placements of the cogs in regards to each other still. Colours are not at all finalised, but I was sick of looking at B & W. :)
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,14 +1,69 @@
 
 function draw_one_frame(cur_frac) {
-  //background(255);
+  // My Code
+  let deepGold = color("#9a6226");
+  let lightGold = color("#ffa500");
 
+  background(deepGold);
+  // cogRotation();
+    let angle = cur_frac*2;
+    let xPosition = width/4
+    let yPosition = height/3
+    
+  translate(xPosition, yPosition);
+  push();
+  rotate(angle);
+  cog();
+  pop();
+  
+  // second cog
+  translate(xPosition, yPosition);
+  push();
+  rotate(-angle + TWO_PI/3);
+  cog();
+  pop();
+}
+
+////////// FUNCTIONS //////////
+// Cog Shape created by ChatGPT.
+function cog() {
+  let numTeeth = 8; // number of teeth on the cog
+  let cogRadius = 100; // radius of the cog
+  let innerRadius = cogRadius * 0.85; // radius of the circle in the center of the cog
+  let toothSize = cogRadius / 12; // length of each tooth
+  let angleStep = TWO_PI / numTeeth; // angle between each tooth
+  let angleOffset = angleStep / 2; // offset the first tooth by half an angleStep
+  // translate(width/2, height/2); // move the origin to the center of the canvas
+
+  beginShape();
+  let midGold = color("#cc8400");
+  let deepGold = color("#9a6226");
+
+  fill(midGold);
+  stroke(deepGold);
+  for (let i = 0; i < numTeeth; i++) {
+    let angle = i * angleStep - angleOffset;
+    vertex(cos(angle) * cogRadius, sin(angle) * cogRadius);
+    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
+    angle += angleStep / 2;
+    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
+    vertex(cos(angle) * innerRadius, sin(angle) * innerRadius);
+    angle += angleStep / 2;
+    vertex(cos(angle) * innerRadius, sin(angle) * innerRadius);
+    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
+  }
+  endShape(CLOSE);
+}
+
+// Main Clock
+function mainClock(){
   // outer circle
-  ellipse(width/2, height/2, height, height);
+   ellipse(width/2, height/2, height, height);
+   fill(midGold);
 
   // inner circle
   ellipse(width/2, height/2, height/1.2, height/1.2);
-
-
-  ellipse(width/1, height/4, height/2, height/2);
-
+  stroke(lightGold);
 }
+
+
