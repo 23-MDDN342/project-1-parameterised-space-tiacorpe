6185810e3f9d81287a37031de216d74a4de27d44
Tia <leticia.corpe@gmail.com>
2023-03-17 10:58:47 +1300

    Starting from Scratch

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300842&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
 
-14/3/2023
-Didn't get as much work done over the weekend as I was ill. However I did a bit more experimentation this week I've decided that I might scrap this idea and go back to the drawing board. My inspiration was the geometric shapes/patterns, so I think I'll revert back to that idea as I'm not enjoying this one much and it's feeling a little repetitive of my previous projects.
+16/3/2023
+After starting from scratch I've started experimenting with some shapes. Using the easing example as a foundation I've added in some rectangles. Still need to adjust them so they don't bounce at the end but remain moving at different times - will likely also change there size/position on the canvas to fit more stuff in.
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,76 +1,128 @@
+//////// MY CODE //////////
+const ease = new p5.Ease();
 
 function draw_one_frame(cur_frac){
-  // My Code
-  let deepGold = color("#9a6226");
-  background(deepGold);
+  // Colors
+  let magenta = color("#ff33e7");
+  let hot_pink = color("#ff3381");
+  let orange = color("#ff4b33");
+  let gold = color("#ffb133");
 
-  let xPosition = width/4
-  let yPosition = height/4
-  let framerate = cur_frac* 4.8
+  background(gold);
 
-  mainClock();
+  // Upper-left Squares
+  draw_rect_1(cur_frac);
+  draw_rect_2(cur_frac);
+  draw_rect_3(cur_frac);
 
-  // Cogs
-  translate(xPosition, yPosition);
-  push();
-  rotate(framerate);
-  cog();
-  pop();
-  
-  // second cog
-  translate(xPosition/2, yPosition/2);
-  push();
-  rotate(-framerate + TWO_PI/3);
-  cog();
-  pop();
 }
 
-////////// FUNCTIONS //////////
-// Cog Shape created by ChatGPT.
-function cog(cur_frac) {
-  let numTeeth = 8; // number of teeth on the cog
-  let cogRadius = 50; // radius of the cog; changes size
-  let innerRadius = cogRadius * 0.85; // radius of the circle in the center of the cog
-  let toothSize = cogRadius / 12; // length of each tooth
-  let angleStep = TWO_PI / numTeeth; // angle between each tooth
-  let angleOffset = angleStep / 2; // offset the first tooth by half an angleStep
-  // translate(width/2, height/2); // move the origin to the center of the canvas
-
-  beginShape();
-  let midGold = color("#cc8400");
-  let deepGold = color("#9a6226");
-
-  fill(midGold);
-  stroke(deepGold);
-  for (let i = 0; i < numTeeth; i++) {
-    let angle = i * angleStep - angleOffset;
-    vertex(cos(angle) * cogRadius, sin(angle) * cogRadius);
-    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
-    angle += angleStep / 2;
-    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
-    vertex(cos(angle) * innerRadius, sin(angle) * innerRadius);
-    angle += angleStep / 2;
-    vertex(cos(angle) * innerRadius, sin(angle) * innerRadius);
-    vertex(cos(angle) * (cogRadius + toothSize), sin(angle) * (cogRadius + toothSize));
+//////// FUNCTIONS //////////
+// Upper left rectangle/square
+function draw_rect_1(cur_frac) {
+  // General movement w/help from example_easing.js
+  let going_right = true;
+  let amount_across = 0;
+  if (cur_frac < 0.5) {
+    going_right = true;
+    amount_across = cur_frac * 2;
   }
-  endShape(CLOSE);
+  else {
+    going_right = false;
+    amount_across = (cur_frac-0.5) * 2;
+  }
+
+  // Easing movement
+  const left_x = int(0.001 * width);
+  const right_x = int(0.3 * width);
+  const ease_amount_across = ease.circularInOut(amount_across); // OK
+
+  if(going_right) {
+    cur_x2 = map(ease_amount_across, 0, 1, left_x, right_x);
+  }
+  else {
+    cur_x2 = map(ease_amount_across, 0, 1, right_x, left_x)
+  }
+
+  // Rectangle/square
+  let r_height = int(0.01 * height); // y-position of rect
+  let r_size = int(0.08*height); // size control
+  let orange = color("#ff4b33"); // orange
+
+  fill(orange);
+  noStroke();
+  rect(cur_x2, r_height, r_size * 2);
 }
 
-// Main Clock
-function mainClock(cur_frac){
-  let lightGold = color("#ffa500");
-  let ellipseSize = width/3
-  let tick = cur_frac* 4.8
-  
-  ellipseMode(CENTER);
-  fill(lightGold);
-  ellipse(width/2, height/2, ellipseSize, ellipseSize);
+// Upper left rectangle/square 2
+function draw_rect_2(cur_frac) {
+  // General movement w/help from example_easing.js
+  let going_right = true;
+  let amount_across = 0;
+  if (cur_frac < 0.5) {
+    going_right = true;
+    amount_across = cur_frac * 2.5;
+  }
+  else {
+    going_right = false;
+    amount_across = (cur_frac-0.5) * 2.5;
+  }
 
-  strokeWeight(2);
-  line(width/2, height/2, 275, 275);
+  // Easing movement
+  const left_x = int(0.001 * width);
+  const right_x = int(0.3 * width);
+  const ease_amount_across = ease.circularInOut(amount_across); // OK
 
-  // line(ellipseSize/2, ellipseSize/2, tick, tick);
+  if(going_right) {
+    cur_x2 = map(ease_amount_across, 0, 1, left_x, right_x);
+  }
+  else {
+    cur_x2 = map(ease_amount_across, 0, 1, right_x, left_x)
+  }
 
+  // Rectangle/square
+  let r_height = int(0.175 * height); // y-position of rect
+  let r_size = int(0.08*height); // size control
+  let orange = color("#ff4b33"); // orange
+
+  fill(orange);
+  noStroke();
+  rect(cur_x2, r_height, r_size * 2);
 }
 
+// Upper left rectangle/square 3
+function draw_rect_3(cur_frac) {
+  // General movement w/help from example_easing.js
+  let going_right = true;
+  let amount_across = 0;
+  if (cur_frac < 0.5) {
+    going_right = true;
+    amount_across = cur_frac * 3;
+  }
+  else {
+    going_right = false;
+    amount_across = (cur_frac-0.5) * 3;
+  }
+
+  // Easing movement
+  const left_x = int(0.001 * width);
+  const right_x = int(0.3 * width);
+  const ease_amount_across = ease.circularInOut(amount_across); // OK
+
+  if(going_right) {
+    cur_x2 = map(ease_amount_across, 0, 1, left_x, right_x);
+  }
+  else {
+    cur_x2 = map(ease_amount_across, 0, 1, right_x, left_x)
+  }
+
+  // Rectangle/square
+  let r_height = int(0.34 * height); // y-position of rect
+  let r_size = int(0.08*height); // size control
+  let orange = color("#ff4b33"); // orange
+
+  fill(orange);
+  noStroke();
+  rect(cur_x2, r_height, r_size * 2);
+}
 
