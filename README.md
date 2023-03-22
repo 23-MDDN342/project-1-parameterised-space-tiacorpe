91ee201c188740970abd915c1b43ec933b964756
Tia <leticia.corpe@gmail.com>
2023-03-22 17:34:14 +1300

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10300842&assignment_repo_type=AssignmentRepo)
 ### 2023 MDDN342 Project 1: Parameterised Space
 
-16/3/2023
-After starting from scratch I've started experimenting with some shapes. Using the easing example as a foundation I've added in some rectangles. Still need to adjust them so they don't bounce at the end but remain moving at different times - will likely also change there size/position on the canvas to fit more stuff in.
+21/3/2023
+Forgot to commit this last session. Have got most of the unrefined movements on the page, though still a couple glitches in those to sort out. Am fairly happy with the composition at the moment but will likely make more tweaks as I further refine things. Currently have a little room on the far right side to add more elements in, but I'm not 100% sure what I want those to be at the moment.
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -8,12 +8,24 @@ function draw_one_frame(cur_frac){
   let orange = color("#ff4b33");
   let gold = color("#ffb133");
 
-  background(gold);
+  background(0);
+  fill(gold);
+  rect(0, 0, width * 4/6, height);
 
-  // Upper-left Squares
+  fill(hot_pink);
+  rect(width * 5/6, 0, width * 1/6, height/2);
+
+  // Back rectangle
+  draw_Backrect(cur_frac);
+
+  // Upper-left squares
   draw_rect_1(cur_frac);
   draw_rect_2(cur_frac);
   draw_rect_3(cur_frac);
+
+  // Rotating semi-circle
+  draw_semi();
+  draw_semi2();
 
 }
 
@@ -33,8 +45,8 @@ function draw_rect_1(cur_frac) {
   }
 
   // Easing movement
-  const left_x = int(0.001 * width);
-  const right_x = int(0.3 * width);
+  const left_x = int(0);
+  const right_x = int(width/6);
   const ease_amount_across = ease.circularInOut(amount_across); // OK
 
   if(going_right) {
@@ -45,13 +57,13 @@ function draw_rect_1(cur_frac) {
   }
 
   // Rectangle/square
-  let r_height = int(0.01 * height); // y-position of rect
-  let r_size = int(0.08*height); // size control
+  let r_height = int(0); // y-position of rect
+  let r_size = int(height/3); // size control
   let orange = color("#ff4b33"); // orange
 
   fill(orange);
   noStroke();
-  rect(cur_x2, r_height, r_size * 2);
+  rect(cur_x2, r_height, r_size);
 }
 
 // Upper left rectangle/square 2
@@ -69,8 +81,8 @@ function draw_rect_2(cur_frac) {
   }
 
   // Easing movement
-  const left_x = int(0.001 * width);
-  const right_x = int(0.3 * width);
+  const left_x = int(0);
+  const right_x = int(width/6);
   const ease_amount_across = ease.circularInOut(amount_across); // OK
 
   if(going_right) {
@@ -81,13 +93,13 @@ function draw_rect_2(cur_frac) {
   }
 
   // Rectangle/square
-  let r_height = int(0.175 * height); // y-position of rect
-  let r_size = int(0.08*height); // size control
+  let r_height = int(height/3); // y-position of rect
+  let r_size = int(height/3); // size control
   let orange = color("#ff4b33"); // orange
 
   fill(orange);
   noStroke();
-  rect(cur_x2, r_height, r_size * 2);
+  rect(cur_x2, r_height, r_size);
 }
 
 // Upper left rectangle/square 3
@@ -105,8 +117,8 @@ function draw_rect_3(cur_frac) {
   }
 
   // Easing movement
-  const left_x = int(0.001 * width);
-  const right_x = int(0.3 * width);
+  const left_x = int(0);
+  const right_x = int(width/6);
   const ease_amount_across = ease.circularInOut(amount_across); // OK
 
   if(going_right) {
@@ -117,12 +129,85 @@ function draw_rect_3(cur_frac) {
   }
 
   // Rectangle/square
-  let r_height = int(0.34 * height); // y-position of rect
-  let r_size = int(0.08*height); // size control
+  let r_height = int(height * 2/3); // y-position of rect
+  let r_size = int(height/3); // size control
   let orange = color("#ff4b33"); // orange
 
   fill(orange);
   noStroke();
-  rect(cur_x2, r_height, r_size * 2);
+  rect(cur_x2, r_height, r_size);
 }
 
+// Backgrounf rectangle
+function draw_Backrect(cur_frac) {
+// General movement w/help from example_easing.js
+  let going_right = true;
+  let amount_across = 0;
+  if (cur_frac < 0.5) {
+    going_right = true;
+    amount_across = cur_frac * 2;
+  }
+  else {
+    going_right = false;
+    amount_across = (cur_frac-0.5) * 2;
+  }
+
+  // Easing movement
+  const left_x = int(width/6);
+  const right_x = int(width/4);
+  const ease_amount_across = ease.circularInOut(amount_across); // OK
+
+  if(going_right) {
+    cur_x2 = map(ease_amount_across, 0, 1, right_x, left_x);
+  }
+  else {
+    cur_x2 = map(ease_amount_across, 0, 1, left_x, right_x)
+  }
+
+  // Rectangle/square
+  let magenta = color("#ff33e7");
+
+  fill(magenta);
+  noStroke();
+  rect(cur_x2, 0, width/6, height);
+}
+
+// Rotating semi-circle
+function draw_semi(cur_frac) {
+  let hot_pink = color("#ff3381");
+
+  // Origin
+  translate(width * 2/3, height/4);
+
+  // Semi-circle
+  let diameter = height/2;
+  let x = 0;
+  let y = 0;
+  
+  // Rotation
+  let angle = radians(frameCount * 20 % 360);
+  rotate(angle);
+
+  fill(hot_pink);
+  arc(x, y, diameter, diameter, 0, PI);
+}
+
+// Rotating semi-circle 2
+function draw_semi2(cur_frac) {
+  let hot_pink = color("#ff3381");
+
+  // Origin
+  translate(width/4, height/4);
+
+  // Semi-circle
+  let di = height/2;
+  let x = 0;
+  let y = 0;
+  
+  // Rotation
+  let angle2 = radians(frameCount * 20 % 360);
+  rotate(angle2);
+
+  fill(hot_pink);
+  arc(x, y, di, di, 0, PI);
+}
